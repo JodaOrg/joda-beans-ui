25c043f3e52a39f122ee616820681961326a64bb
Stephen Colebourne <scolebourne@joda.org>
2013-11-21 11:23:31 +0000

    Launch Swing frame correctly

--- src/test/java/org/joda/beans/ui/swing/SwingUIExample.java
+++ src/test/java/org/joda/beans/ui/swing/SwingUIExample.java
@@ -19,6 +19,7 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
 /**
@@ -29,39 +30,51 @@ public class SwingUIExample {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            Person person = new Person();
-            person.setForename("Stephen");
-            person.setSurname("Colebourne");
-            person.setAge((short) 30);
-            person.setTripCount(6);
-            person.setChild(true);
-            person.setAddress(new Address());
-            person.getAddress().setStreet("186 Park Street");
-            person.getAddress().setCity("London");
-            person.getTags().add("healthy");
-            person.getTags().add("fun");
-            person.getTags().add("employed");
-            person.getTags().add("londoner");
-            person.getTags().add("java developer");
-            person.getTags().add("blogger");
-            person.getTags().add("speaker");
-            person.getTags().add("troublemaker");
-            person.getTags().add("traveller");
-            
-            JPanel panel = new JodaBeanSwingUI().createForm(person);
-            
-            JFrame frame = new JFrame("Swing bean example");
-            JScrollPane scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-            scrollPane.setBorder(BorderFactory.createEmptyBorder());
-            frame.getContentPane().add(scrollPane);
-            frame.setSize(600, 500);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setLocationByPlatform(true);
-            frame.setVisible(true);
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    createUI();
+                }
+            });
         } catch (Exception ex) {
             ex.printStackTrace();
             System.exit(1);
         }
+    }
+
+    private static void createUI() {
+        Person person = createPerson();
+        JPanel panel = new JodaBeanSwingUI().createForm(person);
+        JFrame frame = new JFrame("Swing bean example");
+        JScrollPane scrollPane = new JScrollPane(panel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        frame.getContentPane().add(scrollPane);
+        frame.setSize(600, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    private static Person createPerson() {
+        Person person = new Person();
+        person.setForename("Stephen");
+        person.setSurname("Colebourne");
+        person.setAge((short) 30);
+        person.setTripCount(6);
+        person.setChild(true);
+        person.setAddress(new Address());
+        person.getAddress().setStreet("186 Park Street");
+        person.getAddress().setCity("London");
+        person.getTags().add("healthy");
+        person.getTags().add("fun");
+        person.getTags().add("employed");
+        person.getTags().add("londoner");
+        person.getTags().add("java developer");
+        person.getTags().add("blogger");
+        person.getTags().add("speaker");
+        person.getTags().add("troublemaker");
+        person.getTags().add("traveller");
+        return person;
     }
 
 }
